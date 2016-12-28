d3f25f2b30d4bdabe2a8211b3424d2e241080401
DESKTOP-L8U3P3V\Nasri <narsiyatim@hotmail.com>
2016-12-28 10:39:51 +0200

    Testing2

--- MarketDelivery/MainActivity.cs
+++ MarketDelivery/MainActivity.cs
@@ -10,7 +10,7 @@ namespace MarketDelivery
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
-
+            //Test
             // Set our view from the "main" layout resource
             // SetContentView (Resource.Layout.Main);
         }
